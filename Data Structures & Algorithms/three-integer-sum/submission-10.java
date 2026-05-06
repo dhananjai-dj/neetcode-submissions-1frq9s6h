25b1e0d6cc67523b5acf4752fc227c5cc83966cf
dhananjai-dj <119773597+dhananjai-dj@users.noreply.github.com>
2026-05-06 08:31:11 +0530

    Add: three-integer-sum - submission-10

---
+++ Data Structures & Algorithms/three-integer-sum/submission-10.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int lastIndex = nums.length - 1;
+        for (int i = 0; i <= lastIndex; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = lastIndex;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                    continue;
+                } else if (sum > 0) {
+                    right--;
+                    continue;
+                } else {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                }
+                left++;
+                right--;
+                while (left <= lastIndex && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+                while (right > -1 && nums[right] == nums[right + 1]) {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
